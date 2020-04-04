50ab0c9cd49ebdeb5e03b60c4238ac98595e2999
Logan Donoughe <logandonoughe@gmail.com>
2020-04-03 19:12:30 -0700

    Snake names

--- main.py
+++ main.py
@@ -8,24 +8,32 @@ _\ \ | | | (_| |   <  __/ | | | | | (_| | | | | (_|  __/ |
 '''
 )
 
+class Snake:
+  def __init__(self, name: str, venomous: bool, size: int):
+        self.name = name
+        self.venomous = venomous
+        self.size = size
+
 print('Assemble Your Snake')
 print('Is it venomous?')
 venom = input()
 print('How big is it (in centimeters)?')
-size = input()
+size = int(input())
+# Probably should replace with just checking the first letter
 venomous = venom.lower() in ['true', 'tru', 'truuu', '1', 't', 'y', 'yes', 'yeah', 'yeah buddy', 'fa sho', 'yup']
 print_venom = '' if venomous else 'not '
 print(f'Your snake is {print_venom}venomous and {size} meters')
+print('What would you like to name your snake?')
+name = input()
+snake = Snake(name, venomous, size)
+print(f'{name}\'s reference is {snake}')
 
 
 
 class Store:
   pass
 
-class Snake:
-  def __init__(self, venomous, size):
-        self.venomous = venomous
-        self.size = size
+
 
 class Snakemancer:
   pass
