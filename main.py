78d6413f0e85b6cdcd87727b2e131efdf3afa96b
Logan Donoughe <logandonoughe@gmail.com>
2020-04-06 19:38:11 -0700

    Stops mission progression on failure

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ class Snakemancer:
 
 user = Snakemancer([])
 
+class Store:
+  pass
+
 class Snake:
   def __init__(self, name: str, venomous: bool, size: int):
         self.name = name
@@ -43,31 +46,26 @@ size = int(input())
 # Probably should replace with just checking the first letter
 venomous = venom.lower() in ['true', 'tru', 'truuu', '1', 't', 'y', 'yes', 'yeah', 'yeah buddy', 'fa sho', 'yup']
 print_venom = '' if venomous else 'not '
-print(f'Your snake is {print_venom}venomous and {size} meters')
+print(f'Your snake is {print_venom}venomous and {size} centimeters')
 print('What would you like to name your snake?')
 name = input()
 snake = Snake(name, venomous, size)
 user.add_snake(snake)
 
 difficulty = 0
-while difficulty < 101:
+failure = False
+while (difficulty < 101 and failure != True):
   mission = Mission(difficulty)
   print(f'You must infiltrate the building. It has a difficulty of {mission.difficulty}')
   result = mission.attempt(snake)
   print(f'Mission is a {result}')
+  if result == 'failure':
+    failure = True
   difficulty += 10
 
+if failure:
+  print('Your snake failed the mission, try again later with a better snake')
 # print(user.snakes)
-
-
-
-
-
-
-
-class Store:
-  pass
-
 
                                                              
                                                     
